dee76edb72da6e90698b3e56f86038f1776aa153
harshitb8 <harsbhad12@gmail.com>
2020-04-29 22:43:26 +0000

    binary tree from gfg practice

---
+++ binary_tree/intro.py
@@ -0,0 +1,16 @@
+"""
+this is the code practie for binary tree in gfg tut
+"""
+
+class Node(self):
+
+    def __init__(self, data):
+
+        self.data = data
+        self.left = None
+        self.right = None
+
+class binary_tree(self):
+
+    def __init__(self):
+        self.root = None
