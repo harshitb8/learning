e3178b20ac0ff90e39cce163368ebdfe756c8937
harshitb8 <harsbhad12@gmail.com>
2020-09-28 10:10:59 +0000

    quick sort

--- DS-Seymour/Stack_Queues_Recursion/Quick_sort.py
+++ DS-Seymour/Stack_Queues_Recursion/Quick_sort.py
@@ -5,12 +5,35 @@
 
 def partitioning(un_list):
 
+    """ Finding out the proper place for pivot element is
+        known as partitioning
+    """
+
     i = 0
-    j = len(un_list)
+    j = len(un_list) - 1
 
     pivot = un_list[0]
+    print(un_list)
 
-    while (i<j):
+    for k in range(1):
+
 
         while( un_list[i] >= pivot ):
             i+=1
+
+        while( un_list[j] <= pivot ):
+            j-=1
+
+        if (i<j):
+            temp = un_list[i]
+            un_list[i] = un_list[j]
+            un_list[j] = temp
+        
+        print(un_list)
+             
+
+if __name__ == '__main__':
+
+    un_list = [7, 6, 10, 5, 9, 2, 1, 15, 7]
+
+    partitioning(un_list)
