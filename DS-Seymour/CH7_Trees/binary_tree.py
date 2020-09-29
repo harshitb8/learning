8b9ba12efa49c1cd509d6e13a6b0604d81c9d587
harshitb8 <harsbhad12@gmail.com>
2020-09-29 18:34:04 +0000

    binary tree started

---
+++ DS-Seymour/CH7_Trees/binary_tree.py
@@ -0,0 +1,30 @@
+""" making btree node class for bnode
+    binary tree can point to root
+"""
+
+class bnode:
+
+    def __init__(self, data):
+        
+        self.data = data
+        self.left = left
+        self.right = right
+
+"""
+## ------- testing part build below tree
+#       tree
+#       ----
+#        1    <-- root
+#      /   \
+#     2     3  
+#    /   
+#   4
+"""
+
+if __name__ == '__main__':
+
+    root = bnode(1)
+    root.left = bnode(2)
+    root.right = bnode(3)
+
+    root.left.left = bnode(4)
